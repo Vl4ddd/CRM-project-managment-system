2f3662ecf1d6e54d70ca0ee2a899e1b435c9bc11
Vladislav Ostanin <ostanin8900@yandex.ru>
2024-11-05 20:08:43 +0500

    add report entity

---
+++ crm/src/main/java/com/crmpoject/crm/entities/Report/Report.java
@@ -0,0 +1,35 @@
+package com.crmpoject.crm.entities.Report;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "report")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Report {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; 
+
+    @Enumerated(EnumType.STRING) 
+    private ReportStatus status;
+
+    private String content; 
+
+}
